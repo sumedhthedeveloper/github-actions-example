537bc2ff6741f08d73cefb1d196401f8cb2bda5a
Sumedh Tayade <sumedhtayade@ALIPL5585.local>
2023-03-17 21:07:58 +0530

    Github actions example

---
+++ src/main/java/com/tayadetech/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.tayadetech.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome sumedhthedeveloper !";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
